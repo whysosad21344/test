b49073999874479c6ca108eaf6e7695e418ee955
whysosad21344 <gtt5846@outlook.com>
2025-08-16 19:40:49 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,20 +1,27 @@
+-- main.lua
 local Library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/whysosad21344/test/main/Creator.lua"))()
 
 local Window = Library:CreateWindow({Title="My Script"})
 
 Window:AddButton({
     Text="Click Me!",
-    Callback=function() print("Clicked!") end
+    Callback=function()
+        print("Button clicked!")
+    end
 })
 
 Window:AddToggle({
     Text="Enable Feature",
     Default=false,
-    Callback=function(state) print("Toggle:", state) end
+    Callback=function(state)
+        print("Toggle state:", state)
+    end
 })
 
 Window:AddTextbox({
-    Text="Enter Name",
-    Placeholder="Type here...",
-    Callback=function(input) print("Input:", input) end
+    Text="",
+    Placeholder="Type something...",
+    Callback=function(input)
+        print("Input:", input)
+    end
 })
